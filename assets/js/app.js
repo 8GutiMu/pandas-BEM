ca73367a44f854aed308353261a7523af6116b77
BRENDA <brendagutierrez@iMac-de-Brenda.local>
2017-07-02 22:07:27 -0500

    bem

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
 console.log("hola, ya me he conectado;")
+
+var cargarPagina = function(){
+    agregarAddEventListener();
+}
+
+var agregarAddEventListener = function(){
+    $(".fotos__panda--cerrar").addEventListener(eliminarPanditas);
+}
+
+var eliminarPanditas = function(){
+    
+}
+
+($document).ready(cargarPagina);
